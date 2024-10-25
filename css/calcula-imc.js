2e275c747829673afde354e2c03573313c4475eb
Larissa de Souza Mendes <161725440+larissaSouzaM@users.noreply.github.com>
2024-10-25 11:53:01 -0300

    Update calcula-imc.js

--- css/calcula-imc.js
+++ css/calcula-imc.js
@@ -1,10 +1,37 @@
-if(pesoEhValido && alturaEhValida){
-var imc = calculaImc(peso, altura);
-tdImc.textContent = imc;
-  
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Lucas Nutrição";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < 5; i++) {
+  console.log(pacientes[i]);
+ 
+  var tdPeso = pacientes[i].querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = pacientes[i].querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = pacientes[i].querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso < 0 || peso > 600) {
+  tdPeso.textContent = "Peso inválido!";
+  pesoEhValido = false;
+  pacientes[i].classList.add("dados-invalidos");
 }
-function calculaImc(peso, altura){
-  var imc = 0;
-  imc = peso / (altura * altura)
-  return imc.toFixed(2);
+
+if (altura < 0 || altura > 2.80) {
+  tdAltura.textContent = "Altura inválida!";
+  alturaEhValida = false;
+  pacientes[i].classList.add("dados-invalidos");
 }
+
+if (pesoEhValido && alturaEhValida) {
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc.toFixed(2);
+}
+}
+
